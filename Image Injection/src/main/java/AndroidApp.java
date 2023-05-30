c0c278674e8af80fdcd3cf7bfcd71d4e91df5c9b
Private1647 <shantanuw@lambdatest.com>
2023-05-30 16:10:37 +0530

    Added Geolocation, Image Injection, Multiple Apps & Tunnel

---
+++ Image Injection/src/main/java/AndroidApp.java
@@ -0,0 +1,70 @@
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileElement;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.Test;
+import java.net.URL;
+
+public class AndroidApp {
+
+    String userName = System.getenv("LT_USERNAME") == null ?
+            "shantanuw" : System.getenv("LT_USERNAME"); //Add username here
+    String accessKey = System.getenv("LT_ACCESS_KEY") == null ?
+            "AO8Hw4rmWhqRRYIYw8I5zS3j8BKG6dywIPYysMH2OjGmlUayvB" : System.getenv("LT_ACCESS_KEY"); //Add accessKey here
+
+    public String gridURL = "@mobile-hub.lambdatest.com/wd/hub";
+
+    AppiumDriver driver;
+
+    @Test
+    @org.testng.annotations.Parameters(value = {"device", "version", "platform"})
+    public void AndroidApp1(String device, String version, String platform) {
+        try {
+            DesiredCapabilities capabilities = new DesiredCapabilities();
+            capabilities.setCapability("build","Java TestNG Android - Image Injection");
+            capabilities.setCapability("name",platform+" "+device+" "+version);
+            capabilities.setCapability("deviceName", device);
+            capabilities.setCapability("platformVersion",version);
+            capabilities.setCapability("platformName", platform);
+            capabilities.setCapability("isRealMobile", true);
+            capabilities.setCapability("app", "lt://APP1016039251685436343364197"); //Enter your app url
+            capabilities.setCapability("deviceOrientation", "PORTRAIT");
+            capabilities.setCapability("console", true);
+            capabilities.setCapability("network", false);
+            capabilities.setCapability("visual", true);
+            capabilities.setCapability("devicelog", true);
+            capabilities.setCapability("enableImageInjection", true);
+            capabilities.setCapability("autoGrantPermissions",true);
+
+            String hub = "https://" + userName + ":" + accessKey + gridURL;
+            driver = new AppiumDriver(new URL(hub), capabilities);
+
+            Thread.sleep(3000);
+
+            ((JavascriptExecutor) driver).executeScript("lambda-image-injection=lt://MEDIAb36219bbe5944972afb9e6424a5bd828");
+
+            Thread.sleep(5000);
+
+            MobileElement el3 = (MobileElement) driver.findElementById("com.poc.sample:id/camera2");
+            el3.click();
+
+            MobileElement el4 = (MobileElement) driver.findElementById("com.poc.sample:id/btn_capture");
+            el4.click();
+
+            Thread.sleep(5000);
+
+            driver.quit();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            try{
+                driver.quit();
+            }catch(Exception e1){
+                e.printStackTrace();
+            }
+        }
+
+
+    }
+}
+
---
+++ Multiple Apps/src/main/java/iOSApp.java
@@ -0,0 +1,75 @@
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileBy;
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidElement;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import java.net.URL;
+import org.testng.annotations.Test;
+
+public class iOSApp {
+
+    String userName = System.getenv("LT_USERNAME") == null ?
+            "shantanuw" : System.getenv("username"); //Add username here
+    String accessKey = System.getenv("LT_ACCESS_KEY") == null ?
+            "AO8Hw4rmWhqRRYIYw8I5zS3j8BKG6dywIPYysMH2OjGmlUayvB" : System.getenv("accessKey"); //Add accessKey here
+
+    public String gridURL = "@mobile-hub.lambdatest.com/wd/hub";
+
+    AppiumDriver driver;
+
+    @Test
+    @org.testng.annotations.Parameters(value = {"device", "version", "platform"})
+    public void iOSApp1(String device, String version, String platform) {
+
+        try {
+            DesiredCapabilities capabilities = new DesiredCapabilities();
+            capabilities.setCapability("build","Java TestNG iOS - Multiple Apps");
+            capabilities.setCapability("name",platform+" "+device+" "+version);
+            capabilities.setCapability("deviceName", device);
+            capabilities.setCapability("platformVersion",version);
+            capabilities.setCapability("platformName", platform);
+            capabilities.setCapability("isRealMobile", true);
+            capabilities.setCapability("app", "lt://APP1016045801683102146433114"); //Enter your app url
+            capabilities.setCapability("deviceOrientation", "PORTRAIT");
+            capabilities.setCapability("console", true);
+            capabilities.setCapability("network", false);
+            capabilities.setCapability("visual", true);
+            capabilities.setCapability("devicelog", true);
+            String[] customOtherApps = {"lt://APP1016039251685438264683846"};
+            capabilities.setCapability("otherApps", customOtherApps);
+
+            String hub = "https://" + userName + ":" + accessKey + gridURL;
+            driver = new AppiumDriver(new URL(hub), capabilities);
+
+            WebDriverWait Wait = new WebDriverWait(driver,30);
+
+            Wait.until(ExpectedConditions.presenceOfElementLocated(MobileBy.AccessibilityId("color"))).click();
+            Thread.sleep(1000);
+
+            Wait.until(ExpectedConditions.presenceOfElementLocated(MobileBy.AccessibilityId("Text"))).click();
+            Thread.sleep(3000);
+
+            driver.activateApp("lambdatest.chief.qaTestApp");
+            Thread.sleep(3000);
+
+            driver.isAppInstalled("lambdatest.chief.qaTestApp");
+            Thread.sleep(3000);
+
+            driver.quit();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            try{
+                driver.quit();
+            }catch(Exception e1){
+                e.printStackTrace();
+            }
+        }
+
+
+    }
+}
