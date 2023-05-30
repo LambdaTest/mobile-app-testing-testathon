62f593295b499dc30964669c53e50fc105223cc5
Private1647 <shantanuw@lambdatest.com>
2023-05-30 16:15:21 +0530

    Added pre-installed apps & parallel testing

---
+++ Parallel/src/main/java/AndroidApp.java
@@ -0,0 +1,99 @@
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileBy;
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidElement;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.Test;
+
+import java.net.URL;
+import java.util.List;
+
+public class AndroidApp {
+
+    String userName = System.getenv("LT_USERNAME") == null ?
+            "shantanuw" : System.getenv("LT_USERNAME"); //Add username here
+    String accessKey = System.getenv("LT_ACCESS_KEY") == null ?
+            "AO8Hw4rmWhqRRYIYw8I5zS3j8BKG6dywIPYysMH2OjGmlUayvB" : System.getenv("LT_ACCESS_KEY"); //Add accessKey here
+
+    public String gridURL = "@mobile-hub.lambdatest.com/wd/hub";
+
+    AppiumDriver driver;
+
+    @Test
+    @org.testng.annotations.Parameters(value = {"device", "version", "platform"})
+    public void AndroidApp1(String device, String version, String platform) {
+        try {
+            DesiredCapabilities capabilities = new DesiredCapabilities();
+            capabilities.setCapability("build","Java TestNG Android - Parallel");
+            capabilities.setCapability("name",platform+" "+device+" "+version);
+            capabilities.setCapability("deviceName", device);
+            capabilities.setCapability("platformVersion",version);
+            capabilities.setCapability("platformName", platform);
+            capabilities.setCapability("isRealMobile", true);
+            capabilities.setCapability("app", "lt://APP10160452361681906950785676"); //Enter your app url
+            capabilities.setCapability("deviceOrientation", "PORTRAIT");
+            capabilities.setCapability("console", true);
+            capabilities.setCapability("network", true);
+            capabilities.setCapability("visual", true);
+            capabilities.setCapability("devicelog", true);
+
+            String hub = "https://" + userName + ":" + accessKey + gridURL;
+            driver = new AppiumDriver(new URL(hub), capabilities);
+
+            MobileElement color = (MobileElement) driver.findElementById("com.lambdatest.proverbial:id/color");
+            color.click();
+            Thread.sleep(1000);
+            color.click();
+
+            MobileElement text = (MobileElement) driver.findElementById("com.lambdatest.proverbial:id/Text");
+            text.click();
+
+            MobileElement toast = (MobileElement) driver.findElementById("com.lambdatest.proverbial:id/toast");
+            toast.click();
+
+            MobileElement notification = (MobileElement) driver.findElementById("com.lambdatest.proverbial:id/notification");
+            notification.click();
+            Thread.sleep(2000);
+
+            MobileElement geo = (MobileElement) driver.findElementById("com.lambdatest.proverbial:id/geoLocation");
+            geo.click();
+            Thread.sleep(5000);
+
+            MobileElement home = (MobileElement) driver.findElementByAccessibilityId("Home");
+            home.click();
+
+            MobileElement speedtest = (MobileElement) driver.findElementById("com.lambdatest.proverbial:id/speedTest");
+            speedtest.click();
+            Thread.sleep(5000);
+
+            MobileElement Home = (MobileElement) driver.findElementByAccessibilityId("Home");
+            Home.click();
+
+            MobileElement browser = (MobileElement) driver.findElementByAccessibilityId("Browser");
+            browser.click();
+
+            MobileElement url = (MobileElement) driver.findElementById("com.lambdatest.proverbial:id/url");
+            url.sendKeys("https://www.lambdatest.com");
+
+            MobileElement find = (MobileElement) driver.findElementById("com.lambdatest.proverbial:id/find");
+            find.click();
+
+            ((JavascriptExecutor) driver).executeScript("lambda-hook: {\"action\": \"setTestStatus\",\"arguments\": {\"status\":\"passed\", \"remark\":\"This is a sample remark for a passed test \"}} ");
+            driver.quit();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            try{
+                ((JavascriptExecutor) driver).executeScript("lambda-hook: {\"action\": \"setTestStatus\",\"arguments\": {\"status\":\"failed\", \"remark\":\"This is a sample remark for a failed test \"}} ");
+                driver.quit();
+            }catch(Exception e1){
+                e.printStackTrace();
+            }
+        }
+
+
+    }
+}
---
+++ Pre-installed app/src/main/java/AndroidApp.java
@@ -0,0 +1,62 @@
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileElement;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.Test;
+
+import java.net.URL;
+import java.util.List;
+
+public class AndroidApp {
+
+    String userName = System.getenv("LT_USERNAME") == null ?
+            "shantanuw" : System.getenv("LT_USERNAME"); //Add username here
+    String accessKey = System.getenv("LT_ACCESS_KEY") == null ?
+            "AO8Hw4rmWhqRRYIYw8I5zS3j8BKG6dywIPYysMH2OjGmlUayvB" : System.getenv("LT_ACCESS_KEY"); //Add accessKey here
+
+    public String gridURL = "@mobile-hub.lambdatest.com/wd/hub";
+
+    AppiumDriver driver;
+
+    @Test
+    @org.testng.annotations.Parameters(value = {"device", "version", "platform"})
+    public void AndroidApp1(String device, String version, String platform) {
+        try {
+            DesiredCapabilities capabilities = new DesiredCapabilities();
+            capabilities.setCapability("build","Java TestNG Android - Pre-installed app");
+            capabilities.setCapability("name",platform+" "+device+" "+version);
+            capabilities.setCapability("deviceName", device);
+            capabilities.setCapability("platformVersion",version);
+            capabilities.setCapability("platformName", platform);
+            capabilities.setCapability("isRealMobile", true);
+            capabilities.setCapability("app", "Stock"); //Enter your app url
+            capabilities.setCapability("appPackage", "com.google.android.youtube");
+            capabilities.setCapability("appActivity", "com.google.android.youtube.HomeActivity");
+            capabilities.setCapability("deviceOrientation", "PORTRAIT");
+            capabilities.setCapability("visual", true);
+
+            String hub = "https://" + userName + ":" + accessKey + gridURL;
+            driver = new AppiumDriver(new URL(hub), capabilities);
+
+            Thread.sleep(5000);
+            MobileElement accountIcon = (MobileElement) driver.findElementByAccessibilityId("Account");
+            accountIcon.click();
+            Thread.sleep(2000);
+
+            MobileElement signInButton = (MobileElement) driver.findElementById("com.google.android.youtube:id/button");
+            signInButton.click();
+            Thread.sleep(2000);
+
+            driver.quit();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            try{
+                driver.quit();
+            }catch(Exception e1){
+                e.printStackTrace();
+            }
+        }
+
+
+    }
+}
